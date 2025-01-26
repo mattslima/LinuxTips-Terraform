de83828c75a663c3966bb96190445ef55ceb0c18
Matheus Lima <matheus.lima220@gmail.com>
2025-01-26 00:22:08 -0300

    feat: adicionando backend no s3

--- dia01/provider.tf
+++ dia01/provider.tf
@@ -1,4 +1,9 @@
 terraform {
+  backend "s3" {
+    bucket = "terraform-linuxtips-2025"
+    key    = "backend-aula"
+    region = "us-east-1"
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
